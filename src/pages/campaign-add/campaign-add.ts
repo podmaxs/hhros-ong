c6d561c0eb0e8d8561475cc1a0e66f07ec2f9cdb
Ale Prieto <aleprieto@gmail.com>
2017-05-13 15:42:24 -0300

    Campaign add form.

--- src/pages/campaign-add/campaign-add.ts
+++ src/pages/campaign-add/campaign-add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Campaign } from "../../implements/interfaces/campaign";
 
 // @IonicPage()
 @Component({
@@ -7,14 +8,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 	templateUrl: 'campaign-add.html',
 })
 export class CampaignAddPage {
-	private campaign;
+	private campaign: Campaign;
 
 	constructor(private navCtrl: NavController, private navParams: NavParams) {
-		this.campaign = navParams.get('campaign');
+		this.campaign = {
+			titulo: '',
+			descripcion: '',
+			objetivo: null,
+			imagen: ''
+		}
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad CampaignPage');
+	}
+
+	private campaignAdd() {
+
 	}
 
 }
