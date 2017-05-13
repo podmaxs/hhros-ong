f6f2d8884783e1c02155b040ccfd593f16bff380
podmaxs <podmaxs@gmail.com>
2017-05-13 14:32:36 -0300

    actualizacion de provider de api y page de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,42 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { MyApp }          from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage }      from '../pages/about/about';
+import { ContactPage }    from '../pages/contact/contact';
+import { HomePage }       from '../pages/home/home';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar }      from '@ionic-native/status-bar';
+import { SplashScreen }   from '@ionic-native/splash-screen';
 
+
+import { HttpModule, JsonpModule } from '@angular/http';
+
+
+  //Pages
+  import { Login}       from '../pages/login/login';
+
+
+  //Components
+  import { Form } from '../components/form/form';
+
+
+  //Providers
+  import { Api }  from '../providers/api';
+  
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    Login,
+    Form
   ],
   imports: [
+    HttpModule,
+    JsonpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -29,11 +46,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    Login
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Api,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
